e08686de9d91a5784415b6cba706d6bb92b53a6d
mraoelinjatovo <mraoelinjatovo@gaming.tech>
2026-03-09 15:57:03 +0100

    WindowModule

---
+++ Engine/WindowModule.cpp
@@ -0,0 +1,39 @@
+#include "WindowModule.h"
+#include "Engine.h"
+
+void WindowModule::Init()
+{
+
+    window = new sf::RenderWindow(sf::VideoMode({ 1280, 720 }), "MARIO BROS");
+
+}
+
+void WindowModule::Update()
+{
+
+    while (const std::optional event = window->pollEvent())
+    {
+        if (event->is<sf::Event::Closed>()) {
+            Engine::GetInstance()->Stop();
+            window->close();
+        }
+
+    }
+
+    window->clear();
+
+}
+
+void WindowModule::Render()
+{
+
+    window->display();
+
+}
+
+sf::RenderWindow* WindowModule::GetRenderWindow()
+{
+
+    return window;
+
+}
