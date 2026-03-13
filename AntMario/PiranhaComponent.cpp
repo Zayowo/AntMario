19e0797ea94cc2bd279bc98daa2ad5322a46c32e
Missile <david10sok@outlook.fr>
2026-03-13 14:31:06 +0100

    piranha fait

--- AntMario/PiranhaComponent.cpp
+++ AntMario/PiranhaComponent.cpp
@@ -1,26 +1,54 @@
 #include "PiranhaComponent.h"
+#include <SquareCollider.h>
+#include <Scene.h>
 
 void PiranhaComponent::Init() {
 	// position au sol + offset ?
 	owner->GetTransform().origin = { 0.f, 0.f };
 	yOrigin = owner->GetTransform().pos.y;
+
+	SquareCollider* collider = owner->GetComponent<SquareCollider>();
+
+	//collider->RegisterCallback("Fireball", [this](GameObject* o) { TouchByBall(o); });
+
 }
 
 void PiranhaComponent::Update(float dt)
 {
 	// deplacement du sol sortie complete 'en fonction du sprite
-	if (isMove == false) {
+	if (isMoving == false) {
+		std::cout << cooldown << std::endl;
 		cooldown += dt;
 	}
 
-	if (cooldown > 3.f) {
-		isMove = true;
+	if (cooldown > 2.f) {
+		
+		isMoving = true;
 
+		if (isOut) {
+			owner->GetTransform().pos.y += speed * dt;
+		}
+		else {
+			owner->GetTransform().pos.y -= speed * dt;
+		}
+		
+		y += speed * dt;
 
 		if (y >= max) {
 			y = 0;
 
-			isMove = false;
+			isMoving = false;
+			cooldown = 0.f;
+			isOut = !isOut;
+
+
+
 		}
+
 	}
 }
+
+//void PiranhaComponent::TouchByBall(GameObject* other) {
+//	other->GetScene()->DeleteGameObject(other);
+//	owner->GetScene()->DeleteGameObject(owner);
+//}
--- AntMario/PiranhaComponent.h
+++ AntMario/PiranhaComponent.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <Component.h>
 
-
 class PiranhaComponent : public Component
 {
 private:
 	void Init() override;
 	void Update(float dt) override;
+	//void TouchByBall(GameObject* o);
 
 	float max = 60.f;
 	float y = 0.f;
 	float yOrigin;
 	float speed = 200.f;
-	float cooldown = 0.f; // max 3 sec
-	bool isMove = false;
+	float cooldown = 0.f; // max 2 sec
+	float cooldownMax = 2.f;
+	bool isMoving = false;
+
+	bool isOut = false;
 };
 
